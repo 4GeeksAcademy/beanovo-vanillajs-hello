8f20a7a4a7e8d2e5ac1f9b351a5b9088de3d64c9
BeaNovo <168178819+BeaNovo@users.noreply.github.com>
2024-10-24 16:56:19 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let generatefrase = () => {
+  let subjecte = [
+    "Sa padrina",
+    "Es conco",
+    "Es ca",
+    "Es moix",
+    "Es peixater",
+    "Sa Madona"
+  ];
+  let accio = [
+    "s'ha tirat",
+    "m'ha d'aixonat",
+    "m'ha rapinyat",
+    "m'ha perdut",
+    "m'ha mossegat"
+  ];
+  let esque = ["sa cartera", "sa mà", "es d'allonses", "ses claus", "sa cara"];
+  let aon = ["a sa tenda", "per sa finestra", "a sa riera", "p'es carrer"];
+
+  let subjecteIndex = Math.floor(Math.random() * subjecte.length);
+  let accioIndex = Math.floor(Math.random() * accio.length);
+  let esqueIndex = Math.floor(Math.random() * esque.length);
+  let aonIndex = Math.floor(Math.random() * aon.length);
+
+  return (
+    subjecte[subjecteIndex] +
+    " " +
+    accio[accioIndex] +
+    " " +
+    esque[esqueIndex] +
+    " " +
+    aon[aonIndex]
+  );
+};
+window.onload = () => {
   //write your code here
+
+  document.querySelector("#frase").innerHTML = generatefrase();
   console.log("Hello Rigo from the console!");
 };
